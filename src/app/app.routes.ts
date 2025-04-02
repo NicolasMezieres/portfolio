7b3053de99fd594b4728d4ff3eb1025aa1042933
NicolasMezieres <nicolas.mezieres73@gmail.com>
2025-04-02 10:02:36 +0200

    tailwind install

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  { path: 'contact', component: ContactComponent },
+];
