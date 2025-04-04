6ab492e8442411597edf86929393f0df044b22d3
NicolasMezieres <nicolas.mezieres73@gmail.com>
2025-04-04 23:20:56 +0200

    page home light mode end

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
-
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { jamGithub } from '@ng-icons/jam-icons';
+import { bootstrapGithub, bootstrapLinkedin } from '@ng-icons/bootstrap-icons';
 @Component({
   selector: 'app-home .flex-baseline',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, NgIcon],
+  viewProviders: [
+    provideIcons({ bootstrapLinkedin, bootstrapGithub, jamGithub }),
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
