9aeb99b8dea89423fb0435e5bf065456ebc15ccd
NicolasMezieres <nicolas.mezieres73@gmail.com>
2025-04-06 13:42:04 +0200

    skills page with responsive

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
-
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {
+  svglAngular,
+  svglDocker,
+  svglFigma,
+  svglMongodb,
+  svglMysql,
+  svglNestjs,
+  svglPostgresql,
+  svglPrisma,
+  svglReact,
+  svglSvelte,
+  svglTailwindcss,
+} from '@ng-icons/svgl';
 @Component({
   selector: 'app-skills .flex-baseline',
-  imports: [],
+  imports: [NgIcon],
+  viewProviders: [
+    provideIcons({
+      svglReact,
+      svglTailwindcss,
+      svglSvelte,
+      svglAngular,
+      svglNestjs,
+      svglMysql,
+      svglPostgresql,
+      svglPrisma,
+      svglMongodb,
+      svglFigma,
+      svglDocker,
+    }),
+  ],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css',
 })
